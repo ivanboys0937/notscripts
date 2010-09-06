4aafc70dbaa7c8ac9adcfd79351b60f30b4f1b38
optimalcycling <optimalcycling@67ed3b34-d641-4dda-5a18-31d1b1a1e626>
2010-09-06 09:03:05 +0000

    NotScripts 0.9.6 September 6th, 2010 	New: NotScripts can now also block scripts in ftp:// and file:/// (localhost) sites if given permission in the Google Chrome extensions page. 	New: Added support for blocking old-school frameset/frame elements. 	New: In the drop down menu, NotScripts now shows a link to each domain's Google Safe Browsing page to help you decide to whitelist it or not. 		Note that NotScripts only links to the English version of the Google Safe Browsing page at this point. This should change when NotScripts adds additional languages. 	New: NotScripts now removes the "This site may harm your computer" links from Google Search results. Note: Is not compatible with AutoPagerize type extensions. 	New: Added memory stats for the lists in the Options page to help users gauge how much of the HTML5 local storage quota they are using. 	New: NotScripts now supports blocking modes of blacklist and whitelist with top level sites allowed by default. 	New: A page in the Options page on how to report bugs. 	Improved: The temp allow this site features now need much fewer reloads when used because of a change in the way temp permissions are stored. 	Improved: (From Mike) Windows users can now simply copy and paste the folder path shown on the password status page to get 		to their password file without having to type in their user name. 	Improved: Lowered the memory usage and improved the start up time of the NotScripts drop down menu by removing unused components from the jquery UI theme library. 	Improved: Greatly reduced the computing power required for blocking by implementing sorted lists that can be searched with a binary search like algorithm. 	Improved: Site specific storage for the whitelist is now only overwritten when there is a change made instead of on every page load, thanks to the usage of MD5 hashes. 	Changed: IPv4 addresses must now be an exact match when comparing a url with one in the whitelist. 	Changed: The user is now given the option to reset the HTML5 storage of a website when the storage quota is exceeded or the encryption is incorrect, instead of simply trying to reload the web page. 	Bug Fix: "Allow All Shown" button incorrectly adds a domain to the whitelist even when the domain already exists in the whitelist, leading to duplicate entries. 	Bug Fix: A fix has been implemented for the "Are you sure you want to leave this page?" pop ups some users have reported seeing when inline script mitigation is on. 		There is also improved blocking of this pop up implemented. 	Bug Fix: A few users have reported that they are not able to get the "Allow" button to respond. I have not been able to confirm whether this is an actual bug 		but the NotScripts "Allow" button now calls the background page to carry out the actions so this may resolve it. 	Removed: Made a few code changes so that NotScripts no longer requires cross-origin permissions to run in Google Chrome for improved security.

--- blockStart.js
+++ blockStart.js
@@ -20,6 +20,28 @@
 
 const OUT_PUT_LOG = false;
 
+if (OUT_PUT_LOG)
+{
+	if (window.top === window)
+	{
+		console.log("Top Start: " + window.location.href);
+	}
+	else
+	{
+		console.log("IFrame Start: " + window.location.href);
+	}
+	
+	/*for (var i in localStorage)
+	{
+		console.log("localStorage begin: " + i + "  " + localStorage[i]);
+	}
+
+	for (var i in sessionStorage)
+	{
+		console.log("sessionStorage begin: " + i + "  " + sessionStorage[i]);
+	}*/
+}
+
 // DO NOT reuse the following two names if you reuse this code because it will collide with NotScript!
 // Pick your own random names.
 // Also, be careful about compatibility issues with other extensions that use this code because you don't
@@ -28,7 +50,7 @@ const NAME_NOTSCRIPTS_ALLOWED = "dQGIgCdSC1FWpDUMWx1z";
 const NAME_NOTSCRIPTS_WHITELIST = "WQQAW0JuSt0304877x4l";
 
 const PASSWORD_GOOD = isPasswordGood();
-
+const CURR_EPOCH = (new Date()).getTime(); // in milliseconds	
 var fatalError = false;
 
 // We are giving the user the option of putting
@@ -37,6 +59,7 @@ var fatalError = false;
 // const DO_NOT_BLOCK_OBJECTS = true;
 // const DO_NOT_BLOCK_SCRIPTS = true;
 // const DO_NOT_MITIGATE_INLINE_SCRIPTS = true;
+// const BLOCKING_MODE = "BLACKLIST";
 // into their CHANGE__PASSWORD__HERE.js file to disable each type of blocking.
 const do_not_block_iframes = (((!(typeof DO_NOT_BLOCK_IFRAMES === 'undefined')) && DO_NOT_BLOCK_IFRAMES) ? true : false);
 const do_not_block_embeds = (((!(typeof DO_NOT_BLOCK_EMBEDS === 'undefined')) && DO_NOT_BLOCK_EMBEDS) ? true : false);
@@ -91,106 +114,118 @@ var blockSettings = {
     }
 };
 
-// Set the default whitelist if it doesn't exist
-if (!blockSettings.has(NAME_NOTSCRIPTS_WHITELIST)) {
-	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, []);
-}
-var NotScripts_Whitelist = blockSettings.get(NAME_NOTSCRIPTS_WHITELIST);
-if (!isArray(NotScripts_Whitelist))
+function genEmptyWhitelist()
 {
-	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, []);
-	NotScripts_Whitelist = [];
-}
-	
-var sessionBlockSettings = {
-    has: function(key) {
-		if (PASSWORD_GOOD === PASSWORD_STATUS.okay)
-			return key in sessionStorage;
-		else
-			return null;
-    },
-    get: function(key) {
-        if ((PASSWORD_GOOD === PASSWORD_STATUS.okay) && this.has(key)) {
-			var decryptedData = null;
-			try {
-				decryptedData = sjcl.decrypt(ENCRYPTION_PASSWORD, sessionStorage[key]);
-			} catch (err) {
-				if (OUT_PUT_LOG) console.error("Error in NotScripts - blockStart.js (sessionBlockSettings.get): " + err);
-				fatalError = true;
-				sessionStorage[key] = null;
-				return null;
-			}
-			
-			try {
-				return JSON.parse(decryptedData);
-			} catch(err) {
-				return decryptedData;
-			}			
-        }
-		else
-			return null;
-    },
-    set: function(key, value) {
-		if (PASSWORD_GOOD === PASSWORD_STATUS.okay)
-		{
-			try {
-				sessionStorage[key] = sjcl.encrypt(ENCRYPTION_PASSWORD, JSON.stringify(value));
-			} catch (err) {
-				fatalError = true;
-				/*if (OUT_PUT_LOG) console.error("Error in NotScripts - blockStart.js (sessionBlockSettings.set): " + err);
-				if (err == QUOTA_EXCEEDED_ERR) {
-					alert('sessionStorage quota exceeded for NotScripts on ' + window.location.href);
-				}*/
-			}
-		}
-		else
-			return null;
-    }
-};
-	
-// Set the default allow value if it doesn't exist
-if (!sessionBlockSettings.has(NAME_NOTSCRIPTS_ALLOWED)) {
-	sessionBlockSettings.set(NAME_NOTSCRIPTS_ALLOWED, {"globalAllowAll": false, "tempAllowList": []});
-}
-var NotScripts_Allowed = sessionBlockSettings.get(NAME_NOTSCRIPTS_ALLOWED);
-if (typeof NotScripts_Allowed === 'undefined' || NotScripts_Allowed === null)
-{
-	sessionBlockSettings.set(NAME_NOTSCRIPTS_ALLOWED, {"globalAllowAll": false, "tempAllowList": []});
-	NotScripts_Allowed = {"globalAllowAll": false, "tempAllowList": []};
+	return {"whitelist": [], "whitelistHash": EMPTY_MD5,
+		"globalAllowAll": false, "tempAllowList": [], "tempAllowListHash": EMPTY_MD5, "tempExpiry": 0,
+		"blacklist": [], "blacklistHash": EMPTY_MD5};
 }
 
-if (OUT_PUT_LOG)
+// Set the default whitelist if it doesn't exist
+// Note: We converted to a JSON representation in NotScripts V0.9.6.
+// Before that it was an array so we have to detect and upgrade older data without deleting it.
+if (!blockSettings.has(NAME_NOTSCRIPTS_WHITELIST)) {
+	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, genEmptyWhitelist());
+}
+var NotScripts_Whitelist = blockSettings.get(NAME_NOTSCRIPTS_WHITELIST);
+if (isArray(NotScripts_Whitelist))	// Need to convert to new JSON format and sort
 {
-	if (window.top === window)
+	if (OUT_PUT_LOG) 
+		console.log("site whitelist isArray");
+	if (!sortUrlList(NotScripts_Whitelist))	// in place sort
 	{
-		console.log("Top Start: " + window.location.href);
+		// There was an error sorting that requires us to reset the whitelist
+		NotScripts_Whitelist = genEmptyWhitelist();	
+		blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, NotScripts_Whitelist);
 	}
 	else
 	{
-		console.log("IFrame Start: " + window.location.href);
+		// The whitelistHash will be updated when we get the settings from the background page
+		var emptyList = genEmptyWhitelist();
+		emptyList.whitelist = NotScripts_Whitelist;
+		emptyList.whitelistHash = "";
+		NotScripts_Whitelist = emptyList;	
+		blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, NotScripts_Whitelist);		
 	}
-	
-	/*for (var i in localStorage)
+}
+else if (typeof NotScripts_Whitelist === 'undefined' || NotScripts_Whitelist === null 
+	|| !isArray(NotScripts_Whitelist.whitelist) || !isArray(NotScripts_Whitelist.tempAllowList))	// data is corrupt
+{
+	if (OUT_PUT_LOG) 
+		console.log("site whitelist corrupt");
+	NotScripts_Whitelist = genEmptyWhitelist();	
+	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, NotScripts_Whitelist);
+}
+
+if (NotScripts_Whitelist.tempExpiry > 0 && NotScripts_Whitelist.tempExpiry < CURR_EPOCH)
+{
+	if (OUT_PUT_LOG) 
+		console.log("temp list expired");
+	NotScripts_Whitelist.tempAllowList = [];
+	NotScripts_Whitelist.tempAllowListHash = EMPTY_MD5;
+	NotScripts_Whitelist.globalAllowAll = false;
+	NotScripts_Whitelist.tempExpiry = 0;
+	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, NotScripts_Whitelist);
+}
+
+
+function resetAllStorage(settings)
+{
+	localStorage.clear();
+	sessionStorage.clear();	
+
+	if (PASSWORD_GOOD === PASSWORD_STATUS.okay)
 	{
-		console.log("localStorage begin: " + i + "  " + localStorage[i]);
+		fatalError = false;
+		blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, settings.whitelist);
+		NotScripts_Whitelist = blockSettings.get(NAME_NOTSCRIPTS_WHITELIST);
 	}
 
-	for (var i in sessionStorage)
+	if (settings.reload)
 	{
-		console.log("sessionStorage begin: " + i + "  " + sessionStorage[i]);
-	}*/
+		window.location.reload();
+	}	
 }
 
 var pageSourcesAllowed = new Array();
 var pageSourcesTempAllowed = new Array();
 var pageSourcesForbidden = new Array();
+var pageSourcesUntrusted = new Array();
+var topDomain = getPrimaryDomain(window.location.href);
 
-function isWhitelisted(url) {
-	return islisted(NotScripts_Whitelist, url);	
+function isUntrusted(url)
+{
+	return (blocking_mode === BMODE_TYPES.WHITELIST_ALLOW_TOP_LEVEL ? islisted(NotScripts_Whitelist.blacklist, url) : false);
 }
 
-function isTempAllowListed(url) {
-	return islisted(NotScripts_Allowed.tempAllowList, url);	
+function isWhitelisted(url) {
+	switch(blocking_mode)
+	{
+		case BMODE_TYPES.BLACKLIST:
+		{
+			return !islisted(NotScripts_Whitelist.blacklist, url);
+		}
+		case BMODE_TYPES.WHITELIST_ALLOW_TOP_LEVEL:
+		{
+			if (!islisted(NotScripts_Whitelist.blacklist, url))
+			{
+				return (patternMatches(url, topDomain)) || islisted(NotScripts_Whitelist.whitelist, url);
+			}
+			else
+				return false;
+		}
+		default:	// BMODE_TYPES.WHITELIST
+			return islisted(NotScripts_Whitelist.whitelist, url);
+	}	
+}
+
+function isTempAllowListed(url) {	
+	return islisted(NotScripts_Whitelist.tempAllowList, url);		
+}
+
+function isGloballyAllowed()
+{
+	return NotScripts_Whitelist.globalAllowAll;	
 }
 
 /*
@@ -222,7 +257,7 @@ function blockScripts(event)
 	var currUrl = relativeToAbsoluteUrl(getElUrl(el, elType));
 	
 	// Note: Inline scripts do not fire the beforeLoad event and so cannot be prevented from running
-	if ((!do_not_block_iframes && (elType === EL_TYPE.IFRAME)) || 
+	if ((!do_not_block_iframes && (elType === EL_TYPE.IFRAME || elType === EL_TYPE.FRAME)) || 
 		(!do_not_block_embeds && (elType === EL_TYPE.EMBED)) || 
 		(!do_not_block_objects && (elType === EL_TYPE.OBJECT)) || 
 		(!do_not_block_scripts && (elType === EL_TYPE.SCRIPT)))
@@ -230,14 +265,20 @@ function blockScripts(event)
 		if (OUT_PUT_LOG) 
 			console.log("Before " + currUrl);
 		
-		var mainURL = getPrimaryDomain(elType === EL_TYPE.IFRAME ? window.location.href : currUrl);
+		var mainURL = getPrimaryDomain((elType === EL_TYPE.IFRAME || elType === EL_TYPE.FRAME) ? window.location.href : currUrl);
 		
 		if (OUT_PUT_LOG) 
 			console.log("After " + "   " + mainURL + "   " + NotScripts_Allowed.globalAllowAll + "    " + isWhitelisted(mainURL) + "   " + isTempAllowListed(mainURL));			
 		
-		if ((PASSWORD_GOOD === PASSWORD_STATUS.okay))
+		if (PASSWORD_GOOD === PASSWORD_STATUS.okay)
 		{	
-			if (NotScripts_Allowed.globalAllowAll || isWhitelisted(mainURL))
+			if (isUntrusted(mainURL))
+			{
+				if (pageSourcesUntrusted.indexOf(mainURL) < 0)
+					pageSourcesUntrusted.push(mainURL);	
+				preventAndAddToList(event, mainURL, pageSourcesUntrusted);
+			}
+			else if (isGloballyAllowed() || isWhitelisted(mainURL))  
 			{
 				if (pageSourcesAllowed.indexOf(mainURL) < 0)
 					pageSourcesAllowed.push(mainURL);
@@ -249,17 +290,17 @@ function blockScripts(event)
 			}
 			else
 			{
-				preventAndAddToList(event, mainURL);			
+				preventAndAddToList(event, mainURL, pageSourcesForbidden);			
 			}
 		}
 		else
 		{
-			preventAndAddToList(event, mainURL);
+			preventAndAddToList(event, mainURL, pageSourcesForbidden);
 		}	
 	}
 }
 
-function preventAndAddToList(event, mainURL)
+function preventAndAddToList(event, mainURL, list)
 {
 	/*
 	var el = event.target;
@@ -269,11 +310,14 @@ function preventAndAddToList(event, mainURL)
 	
 	event.preventDefault();
 	
-	if (pageSourcesForbidden.indexOf(mainURL) < 0)
-		pageSourcesForbidden.push(mainURL);	
+	if (list.indexOf(mainURL) < 0)
+		list.push(mainURL);	
 }
 
-function mitigateAndAddToList(mainURL)
+/*
+Note: Mitigation is a stop gap measure for inline scripts unline Google Chrome fires beforeload events for them.
+*/
+function mitigateAndAddToList(mainURL, list)
 {
 	injectAnon(function(){
 		for (var i in window)
@@ -285,8 +329,12 @@ function mitigateAndAddToList(mainURL)
 					case "FUNCTION": 
 						if (window[i] !== window.location)
 						{
-							if (window[i] === window.open || window[i] === window.showModelessDialog)
+							if (window[i] === window.open || (window.showModelessDialog && window[i] === window.showModelessDialog))
 								window[i] = function(){return true;};
+							else if (window[i] === window.onbeforeunload)	// To try to fix onbeforeunload pop ups some users report seeing but I can't replicate.
+								window.onbeforeunload = null;
+							else if (window[i] === window.onunload)
+								window.onunload = null;								
 							else
 								window[i] = function(){return "";};
 						}
@@ -304,8 +352,7 @@ function mitigateAndAddToList(mainURL)
 				switch (jsType.toUpperCase())
 				{					
 					case "FUNCTION":
-						//if (document[i] != document.write)	// uncomment this line if debugging and want to try and print out functions
-							document[i] = function(){return "";};
+						document[i] = function(){return "";};
 						break;					
 				}			
 			}
@@ -336,15 +383,15 @@ function mitigateAndAddToList(mainURL)
 			
 			var oNav = navigator;
 			navigator = function(){return "";};
-			oNav = null;
+			oNav = null;			
 		}
 		catch(err)
 		{}
 		
 	});		
 
-	if (pageSourcesForbidden.indexOf(mainURL) < 0)
-		pageSourcesForbidden.push(mainURL);	
+	if (list.indexOf(mainURL) < 0)
+		list.push(mainURL);	
 }
 
 /*
@@ -353,28 +400,33 @@ We can't "disable" all the core javascript functions, but we try to do the best 
 */
 function mitigateInlineScripts()
 {
-	var mainURL = getPrimaryDomain(window.location.href);
-	
-	if ((PASSWORD_GOOD === PASSWORD_STATUS.okay))
+	if (PASSWORD_GOOD === PASSWORD_STATUS.okay)
 	{	
-		if (NotScripts_Allowed.globalAllowAll || isWhitelisted(mainURL))
+		if (isUntrusted(topDomain))
 		{
-			if (pageSourcesAllowed.indexOf(mainURL) < 0)
-				pageSourcesAllowed.push(mainURL);
+			if (pageSourcesUntrusted.indexOf(topDomain) < 0)
+				pageSourcesUntrusted.push(topDomain);
+
+			mitigateAndAddToList(topDomain, pageSourcesUntrusted);
+		}	
+		else if (isGloballyAllowed() || isWhitelisted(topDomain))
+		{
+			if (pageSourcesAllowed.indexOf(topDomain) < 0)
+				pageSourcesAllowed.push(topDomain);
 		}
-		else if (isTempAllowListed(mainURL))
+		else if (isTempAllowListed(topDomain))
 		{
-			if (pageSourcesTempAllowed.indexOf(mainURL) < 0)
-				pageSourcesTempAllowed.push(mainURL);
+			if (pageSourcesTempAllowed.indexOf(topDomain) < 0)
+				pageSourcesTempAllowed.push(topDomain);
 		}
 		else
 		{
-			mitigateAndAddToList(mainURL);			
+			mitigateAndAddToList(topDomain, pageSourcesForbidden);			
 		}
 	}
 	else
 	{
-		mitigateAndAddToList(mainURL);
+		mitigateAndAddToList(topDomain, pageSourcesForbidden);
 	}		
 }
 
@@ -382,32 +434,41 @@ function updateSettings(settings)
 {
 	if (!(PASSWORD_GOOD === PASSWORD_STATUS.okay))
 		return;
-
-	if (OUT_PUT_LOG) 
-		console.log("updateSettings NotScripts_Allowed.globalAllowAll From " + NotScripts_Allowed.globalAllowAll + " to " + settings.tempVals.globalAllowAll);
-
-	var needToReload = false;
-	if (NotScripts_Allowed.globalAllowAll !== settings.tempVals.globalAllowAll 
-		|| NotScripts_Allowed.tempAllowList.length > 0 
-		|| settings.tempVals.tempAllowList.length > 0)
-	{
-		sessionBlockSettings.set(NAME_NOTSCRIPTS_ALLOWED, settings.tempVals);
-		NotScripts_Allowed = sessionBlockSettings.get(NAME_NOTSCRIPTS_ALLOWED);
 		
-		if (NotScripts_Allowed.globalAllowAll && pageSourcesForbidden.length > 0)
+	var needToReload = false;
+	
+	if (NotScripts_Whitelist.whitelistHash !== settings.whitelist.whitelistHash
+		|| NotScripts_Whitelist.tempAllowListHash !== settings.whitelist.tempAllowListHash
+		|| NotScripts_Whitelist.blacklistHash !== settings.whitelist.blacklistHash
+		|| NotScripts_Whitelist.globalAllowAll !== settings.whitelist.globalAllowAll 
+		|| NotScripts_Whitelist.tempExpiry !== settings.whitelist.tempExpiry) 
+	{
+		blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, settings.whitelist);
+		NotScripts_Whitelist = settings.whitelist;
+		
+		if (NotScripts_Whitelist.globalAllowAll && pageSourcesForbidden.length > 0)
 		{
 			needToReload = true;
-		}
-	}	
-		
-	blockSettings.set(NAME_NOTSCRIPTS_WHITELIST, settings.whitelist);
-	NotScripts_Whitelist = blockSettings.get(NAME_NOTSCRIPTS_WHITELIST);
+		}		
+	}
 	
-	if (!needToReload && !NotScripts_Allowed.globalAllowAll)
+	if (OUT_PUT_LOG) 
+		console.log("after NotScripts_Whitelist.whitelist: " + NotScripts_Whitelist.whitelist);
+	
+	if (!needToReload && !isGloballyAllowed())
 	{
-		for (var i in pageSourcesForbidden)
+		for (var i = 0; i < pageSourcesForbidden.length; i++)
 		{
-			if (isTempAllowListed(pageSourcesForbidden[i]) || isWhitelisted(pageSourcesForbidden[i]))
+			if (isTempAllowListed(pageSourcesForbidden[i]) || isWhitelisted(pageSourcesForbidden[i]) || isUntrusted(pageSourcesForbidden[i]))
+			{
+				needToReload = true;
+				break;
+			}
+		}
+
+		for (var i in pageSourcesUntrusted)
+		{
+			if (!isUntrusted(pageSourcesUntrusted[i]))
 			{
 				needToReload = true;
 				break;
@@ -465,15 +526,18 @@ function updateSettings(settings)
 			}			
 		}
 	}
-
+	
 	if (settings.reload && needToReload && !fatalError)
 	{
-		window.location.reload();
+		if (OUT_PUT_LOG) 
+			console.log("need to update");
+		else
+			window.location.reload();
 	}
 }
 
 
-chrome.extension.sendRequest({"type": "get settings block start", "url": window.location.href, "currAllowed": NotScripts_Allowed.globalAllowAll}, updateSettings);
+chrome.extension.sendRequest({"type": "get settings block start", "url": window.location.href, "currAllowed": isGloballyAllowed()}, updateSettings);
 document.addEventListener("beforeload", blockScripts, true);
 
 // Need to update the globalAllowAll to reflect the tempAllows, too
@@ -483,29 +547,41 @@ chrome.extension.onRequest.addListener(
 	{
 		if (window.top === window)
 		{
-			send({"globalAllowAll": NotScripts_Allowed.globalAllowAll, "pageSourcesAllowed": pageSourcesAllowed, "pageSourcesTempAllowed": pageSourcesTempAllowed,
-				"pageSourcesForbidden": pageSourcesForbidden, "fatalError": fatalError, "url": window.location.href});
+			send({"globalAllowAll": isGloballyAllowed(), 
+				"pageSourcesAllowed": pageSourcesAllowed, "pageSourcesTempAllowed": pageSourcesTempAllowed,
+				"pageSourcesForbidden": pageSourcesForbidden, "pageSourcesUntrusted": pageSourcesUntrusted,
+				"fatalError": fatalError, "url": window.location.href, "topDomain": topDomain, 
+				"topDomainIsWhitelisted": (blocking_mode === BMODE_TYPES.WHITELIST_ALLOW_TOP_LEVEL ? islisted(NotScripts_Whitelist.whitelist, topDomain) : false) });
 		}
 		else
 		{
-			chrome.extension.sendRequest({"type": "get sources response", "globalAllowAll": NotScripts_Allowed.globalAllowAll, 
+			chrome.extension.sendRequest({"type": "get sources response", "globalAllowAll": isGloballyAllowed(), 
 				"pageSourcesAllowed": pageSourcesAllowed, "pageSourcesTempAllowed": pageSourcesTempAllowed,
-				"pageSourcesForbidden": pageSourcesForbidden, "fatalError": fatalError, "url": window.location.href});				
+				"pageSourcesForbidden": pageSourcesForbidden, "pageSourcesUntrusted": pageSourcesUntrusted,
+				"fatalError": fatalError, "url": window.location.href, "topDomain": topDomain, 
+				"topDomainIsWhitelisted": (blocking_mode === BMODE_TYPES.WHITELIST_ALLOW_TOP_LEVEL ? islisted(NotScripts_Whitelist.whitelist, topDomain) : false) });				
 		}
 	} 
 	else if (msg.type === "get sources for top window")	// Used to update the state icon of the website
 	{
 		if (window.top === window)
 		{
-			send({"globalAllowAll": NotScripts_Allowed.globalAllowAll, "pageSourcesAllowedLength": pageSourcesAllowed.length + pageSourcesTempAllowed.length,
-				"pageSourcesForbiddenLength": pageSourcesForbidden.length, "fatalError": fatalError, "thisUrl": window.location.href});
+			send({"globalAllowAll": isGloballyAllowed(), 
+				"pageSourcesAllowedLength": pageSourcesAllowed.length + pageSourcesTempAllowed.length,
+				"pageSourcesForbiddenLength": pageSourcesForbidden.length + pageSourcesUntrusted.length, 
+				"fatalError": fatalError, "url": window.location.href, "topDomain": topDomain});
 		}
 	} 			
 	else if (msg.type === "update settings")
 	{
 		updateSettings(msg);
 		send({});
-	} 			
+	} 	
+	else if (msg.type === "reset all storage")
+	{
+		resetAllStorage(msg);
+		send({});
+	}	
 	else
 		send({});
   });
--- options.js
+++ options.js
@@ -13,36 +13,43 @@ function init() {
 	{
 		case PASSWORD_STATUS.tooShort:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});			
 			$("span[name=pwdProbDescrip]").text("Problem: Password is too short, must be at least " + MIN_PASSWORD_LENGTH + " characters.");
 			break;
 		case PASSWORD_STATUS.tooLong:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});			
 			$("span[name=pwdProbDescrip]").text("Problem: Password is too long, must be no greater than " + MAX_PASSWORD_LENGTH + " characters.");
 			break;
 		case PASSWORD_STATUS.empty:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});			
 			$("span[name=pwdProbDescrip]").text("Problem: Password is empty.");
 			break;
 		case PASSWORD_STATUS.invalidChars:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});			
 			$("span[name=pwdProbDescrip]").text("Problem: Password contains invalid characters.");
 			break;
 		case PASSWORD_STATUS.okay:
 			$('#passwordBad').css({"display": "none"});
 			$('#passwordGood').css({"display": "block"});
+			$('#reloadExt').css({"display": "none"});
 			break;
 		case PASSWORD_STATUS.undefined:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});
 			$("span[name=pwdProbDescrip]").text("Problem: Your password file is missing/invalid characters are present/the syntax is incorrect.");
 			break;			
 		default:
 			$('#passwordBad').css({"display": "block"});
-			$('#passwordGood').css({"display": "none"});	
+			$('#passwordGood').css({"display": "none"});
+			$('#reloadExt').css({"display": "none"});
 			$("span[name=pwdProbDescrip]").text("Problem: Unknown problem with password/file.");
 			break;			
 	}
@@ -57,11 +64,11 @@ function init() {
 		{
 			$('#extUpdated').css({"display": "block"});
 			var newDisplayVersion = bgPage.config.get('currDisplayVersion');
-			$("span[name=updatedToVer]").text(newDisplayVersion);
+			$("span[name=updatedToVer]").text(newDisplayVersion);			
 		}
 		else
 		{
-			$('#extUpdated').css({"display": "none"});
+			$('#extUpdated').css({"display": "none"});			
 		}
 		
 		bgPage.config.set('lastVersion', bgPage.config.get('currVersion'));
@@ -71,35 +78,120 @@ function init() {
 		$('#extUpdated').css({"display": "none"});
 	}
 	
-	$('#whitelist').val(bgPage.whitelist.sort().join('\n'));
+	updateWhitelistDisplay();
+	updateBlacklistDisplay();
+	updateTempAllowListDisplay();
+	updateListStats();
+	
+	switch(blocking_mode)
+	{
+		case BMODE_TYPES.BLACKLIST:
+		{
+			$("span[name=blockingMode]").text("Current Blocking Mode: Blacklist");
+			break;
+		}
+		case BMODE_TYPES.WHITELIST_ALLOW_TOP_LEVEL:
+		{
+			$("span[name=blockingMode]").text("Current Blocking Mode: Whitelist, Allow Top Level");
+			break;
+		}
+		default:	// BMODE_TYPES.WHITELIST
+		{
+			$("span[name=blockingMode]").text("Current Blocking Mode: Whitelist");
+			break;
+		}
+	}	
+	
+	$("span[name=userAgent]").text(navigator.userAgent);
+	
 	$('#labelHeaderVersion').text('Version ' + bgPage.config.get('currDisplayVersion'));
 	
-	// Note: jquery has a bug where the radio buttons can't be rechecked through code
 	$('#radio_ON_ReloadTabs').attr('checked', bgPage.config.get('reloadCurrentTabOnToggle'));
 	$('#radio_OFF_ReloadTabs').attr('checked', !bgPage.config.get('reloadCurrentTabOnToggle'));
 	
 	$('#radio_ON_ShowActionButton').attr('checked', bgPage.config.get('showPageActionButton'));
 	$('#radio_OFF_ShowActionButton').attr('checked', !bgPage.config.get('showPageActionButton'));
 	
+	$('#radio_ON_HideHarmfulSearches').attr('checked', bgPage.config.get('hideHarmfulSearches'));
+	$('#radio_OFF_HideHarmfulSearches').attr('checked', !bgPage.config.get('hideHarmfulSearches'));	
+	
 	if (bgPage.extFatalError)
 		$('#extFatalError').css({"display": "block"});
 }
 
 function save() {
-	bgPage.saveWhitelist({"values": lines($('#whitelist').val())});
+	bgPage.saveWhitelist(lines($('#whitelist').val()));
+	updateWhitelistDisplay();	// To take care of the cases when the storage event is not fired
+	
+	bgPage.saveBlacklist(lines($('#blacklist').val()));
+	updateBlacklistDisplay();
+	
+	bgPage.saveTempAllowList(lines($('#tempAllowList').val()));	
+	updateTempAllowListDisplay();
+
+	updateListStats();
 	
 	bgPage.config.set('reloadCurrentTabOnToggle', $('#radio_ON_ReloadTabs').attr('checked') ? true : false);
 	bgPage.config.set('showPageActionButton', $('#radio_ON_ShowActionButton').attr('checked') ? true : false);
+	bgPage.config.set('hideHarmfulSearches', $('#radio_ON_HideHarmfulSearches').attr('checked') ? true : false);
 }
 
 function handleStorageChangeUpdateLists(event)
 {
-	// Bug: The jquery elements are not refreshing when jquery is used to call save even though the actual data is saved
-	//chrome.extension.getBackgroundPage().console.log(event.key);
+	console.log(event.key);
 	if (event.key === "whitelist")
 	{
-		$('#whitelist').val(bgPage.whitelist.sort().join('\n'));
-	}	
+		updateWhitelistDisplay();
+		updateListStats();
+	}
+	else if (event.key === "blacklist")
+	{
+		updateBlacklistDisplay();
+		updateListStats();
+	}
+	/*
+	else if (event.key === "tempAllowList")	// Bug: sessionStorage does not fire storage events
+	{
+		updateTempAllowListDisplay();
+	}
+	*/
+}
+
+function updateWhitelistDisplay()
+{
+	$('#whitelist').val(bgPage.whitelist.join('\n'));
+}
+
+function updateBlacklistDisplay()
+{
+	$('#blacklist').val(bgPage.blacklist.join('\n'));
+	
+}
+
+function updateTempAllowListDisplay()
+{
+	$('#tempAllowList').val(bgPage.tempAllowList.join('\n'));
+}
+
+function updateListStats()
+{
+	const maxStorageSize = 5 * 1024 * 1024;
+	const encryptIncreaseFact = 3.2;
+	var rawListLengths = bgPage.whitelist.length + bgPage.blacklist.length + bgPage.tempAllowList.length;
+	$("span[name=numListEntries]").text(rawListLengths);
+
+	var listSpace = bgPage.localStorage["whitelist"].length + bgPage.localStorage["blacklist"].length + bgPage.sessionStorage["tempAllowList"].length;
+	$("span[name=listSpace]").text(listSpace);
+	$("span[name=listSpacePct]").text((listSpace / maxStorageSize * 100).toFixed(5));
+	
+	$("span[name=listSizeEncrypted]").text(Math.ceil(listSpace * encryptIncreaseFact));
+	$("span[name=listSizeEncryptedPct]").text((Math.ceil(listSpace * encryptIncreaseFact) / maxStorageSize * 100).toFixed(5));
+	
+	$("span[name=maxStorageSize]").text(maxStorageSize);
+	
+	var currLength = rawListLengths >= 10 ? rawListLengths : 10;
+	listSpace = listSpace >= 150 ? listSpace : 150;
+	$("span[name=estMaxListEntries]").text(Math.floor(currLength / ((listSpace * encryptIncreaseFact) / maxStorageSize)));	
 }
 
 window.addEventListener("storage", handleStorageChangeUpdateLists, false);
